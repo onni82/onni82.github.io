24bb078d8c193b52eeac9ae88e5753f1a9e45d64
onni82 <onni.bucht@me.com>
2025-05-08 14:05:02 +0200

    Removed background easter egg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,11 @@
 import { useState } from "react";
 
 const Footer = () => {
-	const [isClicked, setIsClicked] = useState(false);
-	const handleEasterEggClick = () => {
-		setIsClicked(!isClicked);
-		document.body.style.backgroundColor = isClicked ? "" : "lightblue";
-	}
-
 	return (
 		<footer id="footer">
 			<p>
 				&copy; {new Date().getFullYear()}{" "}
-				<span
-				className={`black-text ${isClicked ? "clicked" : ""}`}
-				id="easteregg"
-				onClick={handleEasterEggClick}>
-					Onni Bucht
-				</span>
+				<span className="black-text">Onni Bucht</span>
 			</p>
 		</footer>
 	);
