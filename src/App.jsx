3b722379e6c37b2c9dcc2b29024a3a5d30aea32b
onni82 <onni.bucht@me.com>
2025-05-08 12:49:41 +0200

    updated imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Coding from "./pages/Coding.jsx";
-import Contact from "./pages/Contact.jsx";
-import Education from "./pages/Education.jsx";
-import Skills from "./pages/Skills.jsx";
-import Work from "./pages/Work.jsx";
-import PageNotFound from "./pages/PageNotFound.jsx";
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import EasterEggModal from "./components/EasterEggModal.jsx";
+import Home from "./pages/Home";
+import Coding from "./pages/Coding";
+import Contact from "./pages/Contact";
+import Education from "./pages/Education";
+import Skills from "./pages/Skills";
+import Work from "./pages/Work";
+import PageNotFound from "./pages/PageNotFound";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import EasterEggModal from "./components/EasterEggModal";
 
 const App = () => {
 	const [menuOpen, setMenuOpen] = useState(false);
