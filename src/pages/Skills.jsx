bc164de2695fa722801161dd350f117a71087b11
onni82 <onni.bucht@me.com>
2025-05-08 13:15:32 +0200

    Reading skills and tools from data.json

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,37 +1,26 @@
 import React from "react";
+import data from '../data.json';
 
 const Skills = () => {
 	return (
 		<>
 			<h1>Skills</h1>
 			<div>
-				<section>
-					<p className="sub-category">Media Skills</p>
-					<p>Music writing, lyric writing, audio engineering &amp; music production</p>
-				</section>
-				<section>
-					<p className="sub-category">Languages</p>
-					<p>English, Swedish &amp; some Korean</p>
-				</section>
+				{data.skills.map((skill) => (
+					<section key={skill.id}>
+						<p className="sub-category">{skill.sub_category}</p>
+						<p>{skill.description}</p>
+					</section>
+				))}
 			</div>
 			<h1>Tools</h1>
 			<div>
-				<section>
-					<p className="sub-category">Operating Systems</p>
-					<p>Windows, macOS, Ubuntu (Desktop &amp; Server)</p>
-				</section>
-				<section>
-					<p className="sub-category">Code Editors/IDE</p>
-					<p>Visual Studio, Sublime Text, Code::Blocks, Eclipse</p>
-				</section>
-				<section>
-					<p className="sub-category">Digital Audio Workstations</p>
-					<p>Avid Pro Tools, Image-Line FL Studio, Apple Logic Pro</p>
-				</section>
-				<section>
-					<p className="sub-category">Video Editing Software</p>
-					<p>Adobe Premiere Pro, Avid Media Composer, Apple Final Cut Pro</p>
-				</section>
+				{data.tools.map((tool) => (
+					<section key={tool.id}>
+						<p className="sub-category">{tool.sub_category}</p>
+						<p>{tool.description}</p>
+					</section>
+				))}
 			</div>
 		</>
 	);
