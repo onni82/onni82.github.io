798feb5d895f1689307d0ef3987ecb6a79f42d0a
onni82 <onni.bucht@me.com>
2025-09-05 20:15:30 +0200

    contact page centered

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,7 +4,7 @@ const Contact: React.FC = () => {
 	return (
 		<>
 			<h1>Contact</h1>
-			<div>
+			<div className="container">
 				<section>
 					<p>Email: <a href="mailto:emanuel.bucht@icloud.com">emanuel.bucht@icloud.com</a>.</p>
 					<p>Instagram: <a href="https://instagram.com/onni82" target="_blank" rel="noreferrer nofollow">@onni82</a> &amp; <a href="https://instagram.com/onbprod" target="_blank" rel="noreferrer nofollow">@onbprod</a>.</p>
