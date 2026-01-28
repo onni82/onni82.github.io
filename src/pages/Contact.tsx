3c7925caf9a2f2389a1089f030bc14f57dd2bf4f
onni82 <onni.bucht@me.com>
2026-01-28 15:17:32 +0100

    Updated contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,7 +7,6 @@ const Contact: React.FC = () => {
 			<div className="container">
 				<section>
 					<p>Email: <a href="mailto:emanuel.bucht@icloud.com">emanuel.bucht@icloud.com</a>.</p>
-					<p>Instagram: <a href="https://instagram.com/onni82" target="_blank" rel="noreferrer nofollow">@onni82</a> &amp; <a href="https://instagram.com/onbprod" target="_blank" rel="noreferrer nofollow">@onbprod</a>.</p>
 				</section>
 			</div>
 		</>
