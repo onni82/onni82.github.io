00a9203bd88ad288330a1bc78e98c2998284f94b
onni82 <onni.bucht@me.com>
2026-05-31 20:00:27 +0200

    Updated home text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = () => {
 				</section>
 				<section className="home-intro">
 					<h1>Hello, I'm Onni Bucht</h1>
-					<p>Full Stack Student · Chas Academy</p>
+					<p>Full Stack Developer · Studied at Chas Academy</p>
 					<p>From Södertälje, Sweden </p>
 					<div className="social-button-group">
 						<a className="social-button" href="https://github.com/onni82" target="_blank" rel="noopener noreferrer">
